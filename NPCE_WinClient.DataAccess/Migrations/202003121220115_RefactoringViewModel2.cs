161659595eeecd90721a4b859137b09f25aa34c3
pasquale.macri <pasquale.macri@FGRML021836>
2020-03-12 14:16:40 +0100

    xx

---
+++ NPCE_WinClient.DataAccess/Migrations/202003121220115_RefactoringViewModel2.cs
@@ -0,0 +1,22 @@
+﻿namespace NPCE_WinClient.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RefactoringViewModel2 : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.Service");
+            AlterColumn("dbo.Service", "Id", c => c.Long(nullable: false, identity: true));
+            AddPrimaryKey("dbo.Service", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.Service");
+            AlterColumn("dbo.Service", "Id", c => c.Int(nullable: false, identity: true));
+            AddPrimaryKey("dbo.Service", "Id");
+        }
+    }
+}
---
+++ NPCE_WinClient.UI/ViewModel/IDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace NPCE_WinClient.UI.ViewModel
+{
+    public interface IDetailViewModel
+    {
+        Task LoadAsync(int? id);
+        bool HasChanges { get; }
+    }
+}
