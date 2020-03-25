68d6b636e09cc00ecfa7086300bea6f6597b3d92
pasquale.macri <pasquale.macri@FGRML021836>
2020-03-25 23:21:55 +0100

    xxccc

---
+++ NPCE_WinClient.DataAccess/Migrations/202003252212003_restart.cs
@@ -0,0 +1,26 @@
+﻿namespace NPCE_WinClient.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class restart : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Servizio", "StatoServizioId", "dbo.StatoServizio");
+            DropIndex("dbo.Servizio", new[] { "StatoServizioId" });
+            AlterColumn("dbo.Servizio", "StatoServizioId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Servizio", "StatoServizioId");
+            AddForeignKey("dbo.Servizio", "StatoServizioId", "dbo.StatoServizio", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Servizio", "StatoServizioId", "dbo.StatoServizio");
+            DropIndex("dbo.Servizio", new[] { "StatoServizioId" });
+            AlterColumn("dbo.Servizio", "StatoServizioId", c => c.Int());
+            CreateIndex("dbo.Servizio", "StatoServizioId");
+            AddForeignKey("dbo.Servizio", "StatoServizioId", "dbo.StatoServizio", "Id");
+        }
+    }
+}
